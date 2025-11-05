585fc0f1093753dc0e786df1c87876ecee919036
Tomas <tomaspapuga78@gmail.com>
2025-11-05 16:51:11 +0100

    Initial commit

---
+++ setup.py
@@ -0,0 +1,4 @@
+from setuptools import setup, find_packages
+setup(name="ff_size_ml", version="0.1.0",
+      packages=find_packages(where="src"), package_dir={"": "src"},
+      python_requires=">=3.10")
